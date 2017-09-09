a7195fc4bf8bce9d439469ce82bcb9833bfc347f
Fabien Benetou (laptop, HP Stream 11) <fabien@benetou.fr>
2017-09-09 17:54:55 +0200

    goal gradual destruction visuals

--- client/js/components/goal.js
+++ client/js/components/goal.js
@@ -3,11 +3,21 @@
 
   AFRAME.registerComponent('goal', {
     init: function() {
+	var obj = "model.obj"
+	var mtl = "materials.mtl"
+	var path="public/assets/models/castle/"
 	var el = this.el;
+	var mesh = document.createElement("a-obj-model")
+	mesh.setAttribute("id", "goal-mesh")
+	mesh.setAttribute("src", path+obj)
+	mesh.setAttribute("mtl", path+mtl)
+	el.appendChild(mesh)
 	var life = document.createElement("a-cylinder")
+	// now to rescale because of parent
 	life.setAttribute("color", "green")
 	life.setAttribute("height", "10")
-	life.setAttribute("position", "0 0 -30")
+	life.setAttribute("scale", "0.05 0.05 0.05")
+	life.setAttribute("position", "0 0 0")
 	el.appendChild(life)
 
 	// could have also used a component function
@@ -15,8 +25,23 @@
 		var height = parseInt( life.getAttribute("height") )
 		height -= 1
 		life.setAttribute("height", height)
-		if (height < 5) 
+		if (height < 6) {
+			path="public/assets/models/castle_lvl1/"
+			mesh.setAttribute("src", path+obj)
+			mesh.setAttribute("mtl", path+mtl)
+		}
+		if (height < 3) {
 			life.setAttribute("color", "red")
+			path="public/assets/models/castle_lvl2/"
+			mesh.setAttribute("src", path+obj)
+			mesh.setAttribute("mtl", path+mtl)
+		}
+		if (height < 1) {
+			life.setAttribute("color", "red")
+			path="public/assets/models/castle_lvl3/"
+			mesh.setAttribute("src", path+obj)
+			mesh.setAttribute("mtl", path+mtl)
+		}
 	});
     },
   });
