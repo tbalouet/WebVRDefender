b04843115fdea8fd73a01ae7496ded2845cec050
Fabien Benetou (laptop, HP Stream 11) <fabien@benetou.fr>
2017-09-09 10:45:35 +0200

    added assets, added alongpath with movement

--- client/js/main.js
+++ client/js/main.js
@@ -5,6 +5,43 @@
   var Util         = require("./util.js");
   require("./components/assign_slot.js");
   require("./components/enemy.js");
+  // currently empty
+
+  // should become a component
+  var spots = [ 
+	{pos: new THREE.Vector3( 0, 1, -1 ), occupied: false},
+	{pos: new THREE.Vector3( 2, 1, -1 ), occupied: false},
+	{pos: new THREE.Vector3( 4, 1, -1 ), occupied: false},
+	{pos: new THREE.Vector3( 1, 1, -2 ), occupied: false},
+	{pos: new THREE.Vector3( 3, 1, -2 ), occupied: false}, 
+	{pos: new THREE.Vector3( 5, 1, -2 ), occupied: false} ]
+  function addSpots(){
+	//console.log(AFRAME.scene[0])
+  }
+  function availableSpots(){
+	var available = 0
+	for (var spot in spots){
+		if (!spots[spot].occupied)
+			available++
+	}
+	return available
+  }
+  function getNextSpot(){
+	  if (availableSpots()>0){
+		for (var spot in spots){
+			if (!spots[spot].occupied){
+				spots[spot].occupied = true
+				return spots[spot].pos
+			}
+		}
+	  }
+  }
+  console.log(addSpots())
+  
+  /*
+  ToDo for game dynamics
+	allow spot to be picked by gazing at it
+  */
 
   window.onConnectCB = function(data){
     let player = document.createElement("a-entity");
