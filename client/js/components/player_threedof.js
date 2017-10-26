d35ebf089a96a25e9cf91838ef9515a54f38ea2d
Thomas Balouet <t.balouet@gmail.com>
2017-10-26 10:49:49 +0200

    added gamepadState to handle GearVR Oculus events

--- client/js/components/player_threedof.js
+++ client/js/components/player_threedof.js
@@ -9,6 +9,18 @@
     },
     init: function() {
       var that = this;
+
+      this.gamepadState = undefined;
+      if(AFRAME.utils.device.isGearVR()){
+        var GamepadState = require("../../lib/gamepadState.js");
+        // See GamepadState.js, this is a simple wrapper around the navigator.getGamepads API with Gear VR input detection
+        this.gamepadState = new GamepadState();
+        // When the gamepadState is updated it will use this callback to trigger any detected Gear VR actions
+        this.gamepadState.ongearvrinput = function (gearVRAction) {
+          that.onClick();
+        };
+      }
+
       this.el.setAttribute("networked", {
         template          : "#tower-template",
         showLocalTemplate : true
@@ -37,6 +49,11 @@
 
       cursorAim.currentTarget.emit("hit", {hitPoints: this.data.hitPoints, origin: this.el});
       cursorAim.currentTarget = undefined;
+    },
+    tick: function(){
+      if(this.gamepadState){
+        this.gamepadState.update();
+      }
     }
   });
 })();
